03a9e1e4d7418b21ed850dd89290484c0a6d9fb3
Ranielly Evellyn <2004ninacunha@gmail.com>
2025-05-29 20:24:35 -0300

    alteração adm

--- src/main/java/com/quiz/quiz_sistema_solar/repository/PerguntaRepository.java
+++ src/main/java/com/quiz/quiz_sistema_solar/repository/PerguntaRepository.java
@@ -1,13 +1,10 @@
 package com.quiz.quiz_sistema_solar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import com.quiz.quiz_sistema_solar.model.Pergunta;
 
 @Repository
 public interface PerguntaRepository extends JpaRepository<Pergunta, Integer> {
 }
-=======
-public interface PerguntaRepository extends JpaRepository<Pergunta, Long> {}
->>>>>>> b717b5e85822ab4b8b67dcaf45b12fdb47072cf7
